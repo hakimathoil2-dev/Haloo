cce2bf38ed399e71a5dd5fcd53012ad598824ff4
athoil <hakimathoil2@gmail.com>
2026-04-04 05:28:53 +0700

    add hello world

--- src/hellowoorld/HelloWoorld.java
+++ src/hellowoorld/HelloWoorld.java
@@ -15,6 +15,7 @@ public class HelloWoorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello World");
     }
     
 }
